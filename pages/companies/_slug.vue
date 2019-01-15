afeac0bdf76b0ecdf69469683c0e29c963059c6c
Eugene Nakoneschniy <evgeniy.nakoneschniy@suttermills.com>
2019-01-15 20:41:25 +0200

    initial commit

---
+++ pages/companies/_slug.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-layout column justify-center align-center>
+    Company Detail
+  </v-layout>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/companies/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <CompanyCard/>
+      <CompanyCard/>
+      <CompanyCard/>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+  import CompanyCard from "../../components/CompanyCard";
+  export default {
+    components: { CompanyCard }
+  }
+</script>
